7c0235bc80a2b1b1b985a3b76be79f5a96662a16
Jacoba1100254352 <jacobdanderson@gmail.com>
2022-12-07 12:48:37 -0700

    timer updates

--- lab9_project/timer.h
+++ lab9_project/timer.h
@@ -2,11 +2,16 @@
 #define TIMER
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void timer_init(float period);
 
 void timer_tick();
 
 uint8_t get_timer_val();
+
+void stop_turn_timer();
+
+bool timer_isexpired();
 
 #endif // TIMER //
